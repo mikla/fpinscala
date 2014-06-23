f187a4ab96990b11c8dcbf5be4ccb02331681390
Artsiom Miklushou <artsiom.miklushou@artezio.com>
2014-06-23 18:57:16 +0300

    Ch 3. Ex 2. Map2(Lift)

--- src/main/scala/exception/Ch3Exercises.scala
+++ src/main/scala/exception/Ch3Exercises.scala
@@ -8,4 +8,20 @@ object Ch3Exercises {
       SomeType(xs.foldLeft(0.0)((z, x) => z + Math.pow(x - avg, 2)) / xs.length)
     } else NoneType
   }
+
+  def insuranceRateQuote(age: Int, numberOfSpeedTickets: Int): Double = ???
+  def parseInsuranceRateQuote(age: String, numberOfSpeedTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try(age.toInt)
+    val optSpeedTickets: Option[Int] = Try(numberOfSpeedTickets.toInt)
+    map2(optAge, optSpeedTickets)(insuranceRateQuote)
+  }
+
+  def Try[A](a: => A): Option[A] =
+      try Some(a)
+      catch {case e: Exception => None}
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(aa => b map (bb => f(aa, bb)))
+  }
+
 }
