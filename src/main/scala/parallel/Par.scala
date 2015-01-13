5135546ec238de17a01059886120e6970c7d4f57
Artsiom Miklushou <mikla.op@gmail.com>
2015-01-13 22:47:01 +0300

    Ch7. fork signature

--- src/main/scala/parallel/Par.scala
+++ src/main/scala/parallel/Par.scala
@@ -6,13 +6,21 @@ trait Par[A] {
 
 object Par {
 
-//  `unit` could begin evaluating its argument immediately ina separate (logical) thread,
-//  or it could simply hold
-//  onto its argument until get is called and begin evaluation then. But note that in this
-//  example, if we want to obtain any degree of parallelism, we require that `unit` begin
-//  evaluating its argument concurrently and return immediately
+  /**
+   * `unit` could begin evaluating its argument immediately ina separate (logical) thread,
+   * or it could simply hold
+   * onto its argument until get is called and begin evaluation then. But note that in this
+   * example, if we want to obtain any degree of parallelism, we require that `unit` begin
+   * evaluating its argument concurrently and return immediately*
+   */
   def unit[A](a: A): Par[A] = ???
+
   def get[A](par: Par[A]): A = ???
 
   def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
+
+  /**
+   * Means that the give Par[A] should run in separate logical thread.
+   */
+  def fork[A](a: => Par[A]): Par[A] = ???
 }
