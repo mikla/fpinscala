bee38a3d3b3eff8392c4b9e08c1eae12a1ecbddf
Artsiom Miklushou <mikla.op@gmail.com>
2015-02-16 22:20:05 +0300

    Ex 7.2

--- src/main/scala/parallel/Parallel.scala
+++ src/main/scala/parallel/Parallel.scala
@@ -7,7 +7,31 @@ object Parallel extends App {
       val (l, r) = ints.splitAt(ints.length / 2)
       val sumL = Par.unit(sum(l))
       val sumR = Par.unit(sum(r))
-      Par.get(sumL) + Par.get(sumR)
+      Par.run(sumL) + Par.run(sumR) // Par.get blocks execution here!
+    }
+  }
+
+  /**
+   * Avoiding pitfalls of combining unit and get.
+   */
+  def sum2(ints: IndexedSeq[Int]): Par[Int] = {
+    if (ints.size <= 1) Par.unit(ints.headOption getOrElse 0)
+    else  {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      Par.map2(sum2(l), sum2(r))(_ + _)
+    }
+  }
+
+  /**
+   * Control parallelism via fork
+   * @param ints
+   * @return
+   */
+  def sum3(ints: IndexedSeq[Int]): Par[Int] = {
+    if (ints.size <= 1) Par.unit(ints.headOption getOrElse 0)
+    else  {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      Par.map2(Par.fork(sum3(l)), Par.fork(sum3(r)))(_ + _)
     }
   }
 
