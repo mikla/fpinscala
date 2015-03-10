f03706c8c603f11b300fe5da3123c4412e41a618
Artsiom Miklushou <mikla.op@gmail.com>
2015-03-10 23:32:42 +0300

    Ch 7. parMap and map.

--- src/main/scala/parallel/Par.scala
+++ src/main/scala/parallel/Par.scala
@@ -36,6 +36,19 @@ object Par {
 
   def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map2(parList, unit(()))((a, _) => a.sorted)
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def sortParViaMap(parList: Par[List[Int]]) = map(parList)(_.sorted)
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ???
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
   /**
    * UnitFuture doesn't perform any computation, It's already done.
    */
