62b9ecec8f7f66e768baf623c55ca99473542ccb
Artsiom Miklushou <mikla.op@gmail.com>
2015-10-17 14:56:52 +0300

    Aux pattern.

---
+++ src/main/scala/patterns/auxp/AuxPlayGround.scala
@@ -0,0 +1,38 @@
+package patterns.auxp
+
+import scalaz.Monoid
+
+trait Foo[A] {
+  type B
+  def value: B
+}
+
+object Foo {
+  type Aux[A0, B0] = Foo[A0] {
+    type B = B0
+  }
+}
+
+
+
+object AuxPlayGround extends App {
+
+  implicit def fi = new Foo[Int] {
+    override type B = String
+    override def value: B = "Foo"
+  }
+
+  implicit def fs = new Foo[String] {
+    override type B = Boolean
+    override def value: B = true
+  }
+
+  def foo[T](t: T)(implicit f: Foo[T]): f.B = f.value
+
+//  def fooZero[T](t: T)(implicit f: Foo[T], monoid: Monoid[f.B]) // do not compile!
+
+  def fooAux[T, R](t: T)(implicit f: Foo.Aux[T, R], m: Monoid[R]): R = m.zero
+
+  foo("")
+
+}
