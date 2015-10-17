c4d7f544ba07e12f3c17f467787c446d38db64a8
Artsiom Miklushou <mikla.op@gmail.com>
2015-10-17 17:01:48 +0300

    Applicative functor.

---
+++ src/main/scala/fsis/part2/Applicative.scala
@@ -0,0 +1,59 @@
+package fsis.part2
+
+import fsis.part1.Functor
+
+import scala.language.higherKinds
+
+trait Applicative[F[_]] extends Functor[F] {
+
+  def pure[A](a: A): F[A]
+
+  // this is very similar to map definition.
+  def apply[A, B](fa: F[A])(ff: F[A => B]): F[B]
+
+  def apply2[A, B, Z](fa: F[A], fb: F[B])(ff: F[(A, B) => Z]): F[Z] =
+    apply(fa)(apply(fb)(map(ff)(f => b => a => f(a, b))))
+
+  override def map[A, B](fa: F[A])(f: A => B): F[B] = apply(fa)(pure(f))
+
+  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] =
+    apply(fa)(map(fb)(b => f(_, b)))
+
+  def map3[A, B, C, Z](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] =
+    apply(fa)(map2(fb, fc)((b, c) => a => f(a, b, c)))
+
+  def map4[A, B, C, D, Z](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => Z): F[Z] = {
+    val abTuple = tuple2(fa, fb)
+    val cdTuple = tuple2(fc, fd)
+    map2(abTuple, cdTuple) {
+      case ((a, b), (c, d)) => f(a, b, c, d)
+    }
+  }
+
+  def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((a, b) => (a, b))
+
+  def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] = map3(fa, fb, fc)((a, b, c) => (a, b, c))
+}
+
+object Applicative {
+
+  implicit val optionApplicative: Applicative[Option] = new Applicative[Option] {
+    override def pure[A](a: A): Option[A] = Some(a)
+
+    override def apply[A, B](fa: Option[A])(ff: Option[A => B]): Option[B] = (fa, ff) match {
+      case (None, _) => None
+      case (Some(a), None) => None
+      case (Some(a), Some(f)) => Some(f(a))
+    }
+  }
+
+  implicit val listApplicative: Applicative[List] = new Applicative[List] {
+    override def pure[A](a: A): List[A] = List(a)
+
+    override def apply[A, B](fa: List[A])(ff: List[A => B]): List[B] = for {
+      a <- fa
+      f <- ff
+    } yield f(a)
+  }
+
+}
---
+++ src/main/scala/fsis/part2/play.sc
@@ -0,0 +1,20 @@
+import fsis.part2.Applicative
+import Applicative.{optionApplicative, listApplicative}
+
+optionApplicative.map(Some(1))(_ + 1)
+
+optionApplicative.map(None: Option[Int])(_ + 1)
+
+listApplicative.map(List(1, 2, 3))(_ + 1)
+
+optionApplicative.map2(Some(1), Some(2))(_ + _)
++
+listApplicative.map2(List(1, 2, 3), List(4, 5, 6))(_ + _)
+
+optionApplicative.map3(Some(1), Some(2), Some(4))(_ + _ + _)
+
+optionApplicative.tuple2(Some(2), Some(3))
+
+listApplicative.tuple2(List(1, 2, 3), List(4, 5, 6))
+
+optionApplicative.map4(Some(1), Some(2), Some(4), Some(5))(_ + _ + _ + _)
