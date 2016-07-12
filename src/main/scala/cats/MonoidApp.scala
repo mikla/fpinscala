7dbfc350cba0adb32fc218473c61d91dc0cc3ed4
Artsiom Miklushou <amiklushou@evolutiongaming.com>
2016-07-13 00:16:33 +0300

    cats.Monoid

---
+++ src/main/scala/cats/MonoidApp.scala
@@ -0,0 +1,32 @@
+package cats
+
+import cats.implicits._
+
+object MonoidApp extends App {
+
+  // Monoid extends Semigroup and adds empty
+
+  Monoid[String].empty // ""
+
+  println {
+    Monoid[Map[String, Int]].combineAll(List(Map("a" -> 1, "b" -> 2), Map("a" -> 3)))
+  }
+
+  val l = List(1, 2, 3, 4, 5)
+
+  l.foldMap(identity) // 15
+  l.foldMap(_.toString) // 12345
+
+  def monoidTuple[A: Monoid, B: Monoid]: Monoid[(A, B)] = new Monoid[(A, B)] {
+    override def empty: (A, B) = (Monoid[A].empty, Monoid[B].empty)
+
+    override def combine(x: (A, B), y: (A, B)): (A, B) = {
+      (Monoid[A].combine(x._1, y._1), Monoid[B].combine(x._2, y._2))
+    }
+  }
+
+  println {
+    l.foldMap(v => (v, v.toString))(monoidTuple)
+  }
+
+}
