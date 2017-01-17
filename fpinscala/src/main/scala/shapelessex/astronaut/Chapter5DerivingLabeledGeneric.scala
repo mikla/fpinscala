e00d670baec47e4f8ecb14759547b2044280074f
Artsiom Miklushou <amiklushou@evolutiongaming.com>
2017-01-17 19:14:55 +0200

    Labelled generic

---
+++ fpinscala/src/main/scala/shapelessex/astronaut/Chapter5DerivingLabeledGeneric.scala
@@ -0,0 +1,82 @@
+package shapelessex.astronaut
+
+import shapeless.labelled.FieldType
+import shapeless.{::, HList, HNil, LabelledGeneric, Lazy, Witness}
+
+sealed trait JsonValue
+case class JsonObject(fields: List[(String, JsonValue)]) extends JsonValue
+case class JsonArray(items: List[JsonValue]) extends JsonValue
+case class JsonString(value: String) extends JsonValue
+case class JsonNumber(Value: Double) extends JsonValue
+case class JsonBoolean(value: Boolean) extends JsonValue
+case object JsonNull extends JsonValue
+
+
+trait JsonEncoder[A] {
+  def encode(value: A): JsonValue
+}
+
+object JsonEncoder {
+  def apply[A](implicit enc: JsonEncoder[A]): JsonEncoder[A] = enc
+
+  def instance[A](func: A => JsonValue): JsonEncoder[A] =
+    new JsonEncoder[A] {
+      override def encode(value: A): JsonValue = func(value)
+    }
+
+  implicit val stringEncoder: JsonEncoder[String] = instance(JsonString)
+  implicit val doubleEncoder: JsonEncoder[Double] = instance(JsonNumber)
+  implicit val intEncoder: JsonEncoder[Int] = instance(d => JsonNumber(d))
+  implicit val booleanEncoder: JsonEncoder[Boolean] = instance(JsonBoolean)
+
+  implicit def listEncoder[A](
+    implicit
+    enc: JsonEncoder[A]): JsonEncoder[List[A]] = instance(list => JsonArray(list.map(enc.encode)))
+
+  implicit def optionEncoder[A](
+    implicit
+    enc: JsonEncoder[A]): JsonEncoder[Option[A]] = instance(opt => opt.map(enc.encode).getOrElse(JsonNull))
+
+}
+
+
+trait JsonObjectEncoder[A] extends JsonEncoder[A] {
+  def encode(value: A): JsonObject
+}
+
+object JsonObjectEncoder {
+  def instance[A](fn: A => JsonObject): JsonObjectEncoder[A] =
+    new JsonObjectEncoder[A] {
+      override def encode(value: A): JsonObject = fn(value)
+    }
+
+  implicit val hnilObjectEncoder: JsonObjectEncoder[HNil] = instance(_ => JsonObject(Nil))
+
+  implicit def hlistObjectEncoder[K <: Symbol, H, T <: HList](
+    implicit
+    witness: Witness.Aux[K],
+    hEncoder: Lazy[JsonEncoder[H]],
+    tEncoder: JsonObjectEncoder[T]
+  ): JsonObjectEncoder[FieldType[K, H] :: T] = {
+    val fieldName = witness.value.name
+    instance { hlist =>
+      val head = hEncoder.value.encode(hlist.head)
+      val tail = tEncoder.encode(hlist.tail)
+      JsonObject((fieldName, head) :: tail.fields)
+    }
+  }
+
+  implicit def genericObjectEncoder[A, H <: HList](
+    implicit
+    generic: LabelledGeneric.Aux[A, H],
+    hEncoder: Lazy[JsonObjectEncoder[H]]
+  ): JsonEncoder[A] = instance(value => hEncoder.value.encode(generic.to(value)))
+
+}
+
+object Chapter5DerivingLabeledGeneric extends App {
+  import JsonObjectEncoder._
+
+  println(JsonEncoder[Employee].encode(Employee("A", 1, true)))
+
+}
---
+++ fpinscala/src/main/scala/shapelessex/astronaut/Chapter5LabeledGeneric.scala
@@ -0,0 +1,43 @@
+package shapelessex.astronaut
+
+import shapeless.{HNil, Witness}
+import shapeless.labelled.{FieldType, KeyTag}
+import shapeless.labelled.field
+import shapeless.syntax.singleton._
+
+object Chapter5LabeledGeneric extends App {
+
+  // literal types
+
+  val x42 = 42.narrow // type of x42 is Int(42).
+  println(x42)
+
+  // phantom types
+
+  val number = 42
+  trait Cherries
+  val numCherries = number.asInstanceOf[Int with Cherries] // We end up with a value that is both
+  // an Int and a Cherries at compile time, and an Int at runtime:
+
+  val number2 = 43
+  val taggedNumber = "number223" ->> number
+
+  field[Cherries](123) // FieldType[Cherries,Int] = 123
+
+  def getFieldName[K, V](value: FieldType[K, V])(
+    implicit
+    witness: Witness.Aux[K]): K = witness.value
+
+  println(getFieldName(taggedNumber)) // only for taggged
+
+  def getFieldValue[K, V](value: FieldType[K, V]): V =
+    value
+
+  println(getFieldValue(taggedNumber))
+
+  //records
+
+  // read type is
+  // FieldType["cat", String] :: FieldType["orange", Boolean] :: HNil
+  val garfield = ("cat" ->> "Garfield") :: ("orange" ->> true) :: HNil
+}
