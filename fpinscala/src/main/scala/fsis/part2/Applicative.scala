7e176bfa2d4379c635d68e8b03ec07cc1285b68a
Artsiom Miklushou <amiklushou@evolutiongaming.com>
2017-06-13 10:34:40 +0300

    Applicative composition and laws.

--- fpinscala/src/main/scala/fsis/part2/Applicative.scala
+++ fpinscala/src/main/scala/fsis/part2/Applicative.scala
@@ -5,7 +5,7 @@ import simulacrum.typeclass
 
 import scala.language.higherKinds
 
-@typeclass trait Applicative[F[_]] extends Functor[F] {
+@typeclass trait Applicative[F[_]] extends Functor[F] { self =>
 
   def pure[A](a: A): F[A]
 
@@ -34,6 +34,17 @@ import scala.language.higherKinds
   def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((a, b) => (a, b))
 
   def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] = map3(fa, fb, fc)((a, b, c) => (a, b, c))
+
+  def flip[A, B](ff: F[A => B]): F[A] => F[B] = fa => apply(fa)(ff)
+
+  def compose[G[_]](implicit G: Applicative[G]): Applicative[Lambda[X => F[G[X]]]] =
+    new Applicative[Lambda[X => F[G[X]]]] {
+      def pure[A](a: A): F[G[A]] = self.pure(G.pure(a))
+      def apply[A, B](fga: F[G[A]])(ff: F[G[A => B]]): F[G[B]] = {
+        val x: F[G[A] => G[B]] = self.map(ff)(gab => G.flip(gab))
+        self.apply(fga)(x)
+      }
+    }
 }
 
 object Applicative {
@@ -56,5 +67,33 @@ object Applicative {
       f <- ff
     } yield f(a)
   }
+
+}
+
+trait ApplicativeLaws[F[_]] {
+  import Applicative.ops._
+
+  implicit def F: Applicative[F]
+
+  def applicativeIdentity[A](fa: F[A]) =
+    fa.apply(F.pure((a: A) => a)) == fa // =?=
+
+  def applicativeHomomorphism[A, B](a: A, f: A => B) =
+    F.pure(a).apply(F.pure(f)) == F.pure(f(a)) // =?=
+
+  def applicativeInterchange[A, B](a: A, ff: F[A => B]) =
+     F.pure(a).apply(ff) == ff.apply(F.pure((f: A => B) => f(a)))
+
+  def applicativeMap[A, B](fa: F[A], ff: A => B) =
+    fa.map(ff) == fa.apply(F.pure(ff))
+
+}
+
+object ApplicativeApp extends App {
+
+  import Applicative._
+
+  println(optionApplicative.map3(Option(1), Option(2), Option(3))(_ + _ + _))
+  println(optionApplicative.map3(Option(1), Option(2), None: Option[Int])(_ + _ + _))
 
 }
