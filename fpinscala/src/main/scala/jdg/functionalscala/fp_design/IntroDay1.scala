cea95a5447ac5240ff5abb74867d12b91d1ff595
Artsiom Miklushou <mikla.op@gmail.com>
2020-07-25 17:56:18 +0300

    Refined.

---
+++ fpinscala/src/main/scala/jdg/functionalscala/fp_design/IntroDay1.scala
@@ -0,0 +1,30 @@
+package jdg.functionalscala.fp_design
+
+import java.time.LocalDate
+
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.boolean.And
+import eu.timepit.refined.numeric.{Greater, Less, Positive}
+import eu.timepit.refined.string.MatchesRegex
+import zio.{Queue, ZIO}
+import eu.timepit.refined._
+import eu.timepit.refined.types.string.NonEmptyString
+
+object IntroDay1 extends App {
+
+  def shard[R, E, A](
+    queue: Queue[A],
+    count: Int,
+    worked: A => ZIO[R, E, A]) = ???
+
+  case class CardNumber(value: String Refined MatchesRegex[W.`"^4[0-9]{12}(?:[0-9]{3})?$"`.T])
+  case class SecurityCode(value: Int Refined Greater[W.`99`.T] And Less[W.`1000`.T])
+
+  case class CreditCard(
+    number: CardNumber,
+    name: NonEmptyString,
+    expirationDate: LocalDate,
+    securityCode: SecurityCode
+  )
+
+}
